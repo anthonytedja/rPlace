bac8951518c0d149d1ae42dd03f1cbbeeee2b8ae
Peter Albu <pecasdev@gmail.com>
2023-11-23 20:19:57 -0500

    Begin implementing cache

--- src/api/cache/cache.ts
+++ src/api/cache/cache.ts
@@ -1,4 +1,4 @@
-import { Color } from "../../domain/color";
+import { Color } from '../../domain/color'
 
 export interface Cache {
   set(xPos: number, yPos: number, color: Color): void
--- src/api/cache/impl/dev-cache.ts
+++ src/api/cache/impl/dev-cache.ts
@@ -1,7 +1,19 @@
-import { Color } from "../../../domain/color";
-import { Cache } from "../cache";
+import { createClient as redisCreateClient } from 'redis'
+import { Color } from '../../../domain/color'
+import { Cache } from '../cache'
 
 export class DevCache implements Cache {
+  constructor() {
+    await createClient()
+  }
+
+  async createClient() {
+    await redisCreateClient()
+      .on('error', (err) => console.log('REDIS CLIENT ERROR', err))
+      .connect()
+      .then()
+  }
+
   set(xPos: number, yPos: number, color: Color): void {
     // do some redis shit with docker compose
   }

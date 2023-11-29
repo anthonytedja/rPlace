a0af3e2f6127506bce5891c736f564e3a037f13d
Peter Albu <pecasdev@gmail.com>
2023-11-28 19:25:10 -0500

    Comment out AwsCache

--- src/api/cache/impl/aws-cache.ts
+++ src/api/cache/impl/aws-cache.ts
@@ -1,15 +1,15 @@
 import { Board } from '../../../domain/board'
 import { Cache } from '../cache'
 
-export class AwsCache implements Cache {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  set(xPos: number, yPos: number, colorIdx: number): Promise<void> {
-    // TODO
-    return new Promise(() => console.log('TODO'))
-  }
+// export class AwsCache implements Cache {
+//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//   set(xPos: number, yPos: number, colorIdx: number): Promise<void> {
+//     // TODO
+//     return new Promise(() => console.log('TODO'))
+//   }
 
-  getBoard(): Promise<Board> {
-    // TODO
-    return new Promise(() => console.log('TODO'))
-  }
-}
+//   getBoard(): Promise<Board> {
+//     // TODO
+//     return new Promise(() => console.log('TODO'))
+//   }
+// }

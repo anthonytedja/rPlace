013dc79c662ae10a35ddcf1c38d3852fa394ede5
Anthony Tedja <anthonytedja27@gmail.com>
2023-12-06 01:56:39 -0500

    Add Base Pub Sub (Broken)

--- src/socket/connection.ts
+++ src/socket/connection.ts
@@ -1,70 +1,45 @@
-import WebSocket from 'ws'
-
+import { io, Socket } from 'socket.io-client'
 import { SocketServer } from './socket-server'
 
 export class Connection {
-  constructor(
-    public remoteAddress: string,
-    public websocket: WebSocket,
-    public socketServer: SocketServer
-  ) {
-    console.log('connection init')
-    this.setBindings()
+  socket: Socket
+  server: SocketServer
+  remoteAddress: string
+
+  constructor(server: SocketServer, remoteAddress: string) {
+    console.log('new connection')
+    this.server = server
+    this.remoteAddress = remoteAddress
+    this.socket = io()
+    this.socket.on('connect', () => {
+      this.socket.emit('subscribe', 'board')
+
+      this.socket.on('message', async (message) => {
+        console.log('Received message: ', message)
+
+        var data = JSON.parse(message)
+        const { x, y, color: colorIdx } = data
+        if (x === undefined || y === undefined || colorIdx === undefined) {
+          console.log(`garbage data from connection: ${message}`)
+        }
+        console.log('message: ', x, y, colorIdx)
+
+        if (this.server.board.isValidSet(x, y, colorIdx)) {
+          console.log('valid set')
+
+          const canUpdate = await this.server.userHandler.canUpdate(this.remoteAddress)
+          if (!canUpdate) {
+            console.log('too soon')
+            return
+          }
+
+          await server.cache.set(x, y, colorIdx)
+          await server.board.setPixel(x, y, colorIdx, this.remoteAddress)
+          this.socket.emit('send', 'board', message)
+        } else {
+          console.log('invalid set')
+        }
+      })
+    })
   }
-
-  isAlive: boolean = true
-
-  async setBindings() {
-    //setInterval(checkPing(this), 30000)
-    this.websocket.on('message', await onMessage(this))
-    this.websocket.on('pong', onPong(this))
-  }
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function checkPing(c: Connection) {
-  return () => {
-    if (c.websocket.readyState == WebSocket.OPEN) {
-      if (!c.isAlive) {
-        c.websocket.terminate()
-        return
-      }
-
-      c.isAlive = false
-      c.websocket.ping()
-    }
-  }
-}
-
-async function onMessage(c: Connection) {
-  return async (message: string) => {
-    console.log('raw message:', message)
-
-    var data = JSON.parse(message)
-    const [x, y, colorIdx] = [data.x, data.y, data.color]
-    if (x == undefined || y == undefined || colorIdx == undefined) {
-      console.log(`garbage data from connection: ${message}`)
-    }
-    console.log('message: ', x, y, colorIdx)
-
-    if (c.socketServer.board.isValidSet(x, y, colorIdx)) {
-      console.log('valid set')
-
-      const canUpdate = await c.socketServer.userHandler.canUpdate(c.remoteAddress)
-      if (!canUpdate) {
-        console.log('too soon')
-        return
-      }
-
-      c.socketServer.broadcast(message)
-      await c.socketServer.cache.set(x, y, colorIdx)
-      await c.socketServer.board.setPixel(x, y, colorIdx, c.remoteAddress)
-    } else {
-      console.log('invalid set')
-    }
-  }
-}
-
-function onPong(c: Connection) {
-  return () => (c.isAlive = true)
 }

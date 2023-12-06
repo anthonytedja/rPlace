e0eca6c9104220d6e7a2a4e5ba74162072c7fa7a
Kevin Shin <nivek7221@gmail.com>
2023-12-06 15:28:01 -0500

    Revert "Add Base Pub Sub (Broken)"
    This reverts commit 013dc79c662ae10a35ddcf1c38d3852fa394ede5.
    
     - Revert SocketIO to WebSocket
     - Revert Redis pub/sub implementation

--- src/socket/socket-server.ts
+++ src/socket/socket-server.ts
@@ -1,70 +1,71 @@
+import WebSocket from 'ws'
+
 import { Database } from '../api/database/database'
 import { Cache } from '../api/cache/cache'
 import { Board } from '../domain/board'
+
 import { Connection } from './connection'
 import { UserHandler } from '../domain/user-handler'
 
-import { createClient, RedisClientType } from 'redis'
-
-import { createServer, Server as HttpServer } from 'http'
-import { Server as IoServer } from 'socket.io'
-import { createShardedAdapter } from '@socket.io/redis-adapter'
-
 export class SocketServer {
+  wss: WebSocket.Server
   database: Database
   cache: Cache
   board: Board = new Board()
   userHandler: UserHandler
-  private publisher: RedisClientType
-  private subscriber: RedisClientType
-
-  private httpServer: HttpServer
-  private io: IoServer
 
   async setup(): Promise<void> {
-    console.log('setting up socket server')
     const data = await this.database.getAndFormatBoard()
     this.board = new Board(data)
-    await this.cache.setBoard(this.board)
-    await Promise.all([this.publisher.connect(), this.subscriber.connect()]).then(() => {
-      console.log('redis connected')
-      this.io.adapter(createShardedAdapter(this.publisher, this.subscriber))
-      this.io.on('connection', (socket) => {
-        const remoteAddress = socket.handshake.address || 'unknown'
-        console.log(`connection from ${remoteAddress}`)
-        new Connection(this, remoteAddress)
-
-        socket.on('unsubscribe', (channel) => {
-          console.log('unsubscribing from channel', channel)
-          socket.leave(channel)
-        })
-
-        socket.on('send', (channel, message) => {
-          console.log('sending message', message, 'to channel', channel)
-          this.io.to(channel).emit('message', message)
-        })
-      })
-      this.io.listen(8081)
-    })
-
-    console.log('socket server setup complete')
+    return this.cache.setBoard(this.board)
   }
 
   constructor(database: Database, cache: Cache) {
-    this.httpServer = createServer()
-    this.io = new IoServer(this.httpServer)
-
+    this.wss = new WebSocket.Server({ port: 8081 })
     this.database = database
     this.cache = cache
     this.userHandler = new UserHandler(this.database)
-
-    this.publisher = createClient({ url: 'redis://redis:6379' })
-    this.subscriber = this.publisher.duplicate()
-    console.log('created socket object')
+    this.setBindings()
+    //this.setupPubSub()
   }
 
+  // setupPubSub() {
+  //   this.cache.subscribe('websocket-broadcast', (channel, message) => {
+  //     if (channel === 'websocket-broadcast') {
+  //       console.log('broadcasting', message)
+  //       this.broadcast(message)
+  //     }
+  //   })
+  // }
+
   async getBoard(): Promise<Board> {
-    console.log('getting board')
     return this.cache.getBoard()
+  }
+
+  private setBindings() {
+    this.wss.on('close', onClose(this))
+    this.wss.on('connection', onConnection(this))
+  }
+
+  broadcast(data: string) {
+    this.wss.clients.forEach((client) => {
+      if (client.readyState == WebSocket.OPEN) {
+        client.send(data)
+      }
+    })
+    //this.cache.publish('websocket-broadcast', data)
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function onClose(wss: SocketServer) {
+  return () => console.log('disconnected')
+}
+
+function onConnection(wss: SocketServer) {
+  return (webSocket: WebSocket, request: any) => {
+    const remoteAddress = request.socket.remoteAddress || 'unknown'
+    console.log(`connection from ${remoteAddress}`)
+    new Connection(remoteAddress, webSocket, wss)
   }
 }
